fd5822efef7e2a5124aa1cbd3868c7dc441a06f7
dianamatkava <dianamatkava@gmail.com>
2024-09-13 15:59:37 +0200

    init

--- app/ui/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <main>
-      this is footer
-    </main>
-  );
-}
--- app/ui/menu.tsx
+++
@@ -1,7 +0,0 @@
-export default function Menu() {
-  return (
-    <main>
-      this is Menu
-    </main>
-  );
-}
