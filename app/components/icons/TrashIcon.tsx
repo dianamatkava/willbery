64c05de1cf9f0e01a4b818597a1fde41c7564ab4
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-14 23:59:05 +0200

    Add static header

---
+++ app/components/icons/TrashIcon.tsx
@@ -0,0 +1,30 @@
+interface Props {
+  color: string;
+  className?: string;
+}
+
+export const TrashIcon = ({
+  color = "#4A4A4A",
+  className = "",
+}: Props): JSX.Element => {
+  return (
+    <svg
+      className={`${className}`}
+      fill="none"
+      height="32"
+      viewBox="0 0 32 32"
+      width="32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 7.99984H6.66667M6.66667 7.99984H28M6.66667 7.99984L6.66667 26.6665C6.66667 27.3737 6.94762 28.052 7.44772 28.5521C7.94781 29.0522 8.62609 29.3332 9.33333 29.3332H22.6667C23.3739 29.3332 24.0522 29.0522 24.5523 28.5521C25.0524 28.052 25.3333 27.3737 25.3333 26.6665V7.99984M10.6667 7.99984V5.33317C10.6667 4.62593 10.9476 3.94765 11.4477 3.44755C11.9478 2.94746 12.6261 2.6665 13.3333 2.6665H18.6667C19.3739 2.6665 20.0522 2.94746 20.5523 3.44755C21.0524 3.94765 21.3333 4.62593 21.3333 5.33317V7.99984"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+};
+
+export default TrashIcon;
--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,21 @@
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import {
-  Form,
+  // Form,
   Links,
   // Link,
-  NavLink, // used to indecate the active link, when the URL that matches <NavLink to>, then isActive=true
+  // NavLink, // used to indecate the active link, when the URL that matches <NavLink to>, then isActive=true
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
+  // useLoaderData,
   useNavigation, // returns the current navigation state: it can be one of "idle", "loading" or "submitting".
-  useSubmit,
+  // useSubmit,
 } from "@remix-run/react";
+
+import SideBar from "./components/SideBar";
 
 import appStylesHref from "./app.css?url";
 
@@ -36,97 +38,32 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts, q } = useLoaderData<typeof loader>();
+  // const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
-  const submit = useSubmit();
+  // const submit = useSubmit();
   const searching =
     navigation.location &&
     new URLSearchParams(navigation.location.search).has("q");
 
-  useEffect(() => {
-    const searchField = document.getElementById("q");
-    if (searchField instanceof HTMLInputElement) {
-      searchField.value = q || "";
-    }
-  }, [q]);
+  // useEffect(() => {
+  //   const searchField = document.getElementById("q");
+  //   if (searchField instanceof HTMLInputElement) {
+  //     searchField.value = q || "";
+  //   }
+  // }, [q]);
 
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Willbery</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form
-              id="search-form"
-              role="search"
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch,
-                });
-              }} // submit as you type, handle history
-            >
-              <input
-                id="q"
-                aria-label="Search contacts"
-                defaultValue={q || ""}
-                placeholder="Search"
-                type="search"
-                name="q"
-                className={searching ? "loading" : ""}
-              />
-              <div id="search-spinner" aria-hidden hidden={!searching} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink
-                      className={({ isActive, isPending }) =>
-                        isActive ? "active" : isPending ? "pending" : ""
-                      }
-                      to={`contacts/${contact.id}`}
-                    >
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? <span>★</span> : null}
-                    </NavLink>
-                    {/* <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? <span>★</span> : null}
-                    </Link> */}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
+        <SideBar />
+
         <div
           id="detail"
           className={
