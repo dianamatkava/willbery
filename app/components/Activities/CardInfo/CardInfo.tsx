281ae9b6f64354fb5da26bb60bb800704d58f346
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-17 15:19:09 +0200

    feat/WLLB-1: Fix Card Content css

--- app/components/Activities/CardInfo/CardInfo.tsx
+++ app/components/Activities/CardInfo/CardInfo.tsx
@@ -2,11 +2,12 @@ import CardInfoHeader from "./CardInfoHeader";
 import CardInfoImage from "./CardInfoImage";
 import CardInfoProgress from "./CardInfoProgress";
 
-export default function CardInfo() {
+export default function CardInfo({ imgPath }: { imgPath: string }) {
+  //+
   return (
-    <div className="w-full flex flex-col items-center gap-2 max-h-[70px]">
+    <div className="w-full flex flex-col items-center gap-2 max-h-[145px]">
       <CardInfoHeader />
-      <CardInfoImage />
+      <CardInfoImage imgPath={imgPath} />
       <CardInfoProgress />
     </div>
   );
--- app/components/Activities/CardInfo/CardInfoImage.tsx
+++ app/components/Activities/CardInfo/CardInfoImage.tsx
@@ -1,10 +1,10 @@
-export default function CardInfoImage() {
+export default function CardInfoImage({ imgPath }: { imgPath: string }) {
   return (
     <div className="flex items-center justify-center w-full max-h-[70px]">
       <img
         className="display: block; object-fit: contain; border-radius: 1px 1px 0px 0px; width: 100%; height: 192.375px; object-position: center 50%;"
         alt="Rectangle"
-        src="aws.png"
+        src={imgPath}
       />
     </div>
   );
