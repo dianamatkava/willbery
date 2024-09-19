c717932348f7d4cedb93fd91123d2995b7e7a008
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-19 12:12:22 +0200

    Add Card Filters

--- app/routes/activities/index.tsx
+++ app/routes/activities/index.tsx
@@ -1,5 +1,5 @@
-import ActivitiesFilter from "~/components/Activities/ActivitiesFilter";
-import ActivitiesCard from "../../components/Activities/ActivitiesCard";
+import CardListHeader from "~/components/Cards/CardListHeader/CardListHeader";
+import ActivitiesCard from "../../components/Cards/ActivitiesCard";
 import { json } from "@remix-run/node";
 import { getActivities } from "~/data";
 import { useLoaderData } from "@remix-run/react";
@@ -12,10 +12,10 @@ export const loader = async () => {
 export default function Activities() {
   const { activities } = useLoaderData<typeof loader>();
   return (
-    <div className="w-full flex flex-col items-center gap-5 relative ml-2">
-      <ActivitiesFilter />
-      <div className="flex flex-col items-center w-full px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xlg:grid-cols-5 xxlg:grid-cols-6 gap-2 w-full max-w-[300px] sm:max-w-full">
+    <div className="w-full flex flex-col items-center gap-5 relative">
+      <CardListHeader />
+      <div className="flex flex-col items-center w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xlg:grid-cols-5 xxlg:grid-cols-6 gap-2 w-full max-w-full m-auto ">
           {activities &&
             activities.map((card) => (
               <ActivitiesCard
