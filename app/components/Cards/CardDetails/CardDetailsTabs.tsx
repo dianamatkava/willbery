2cfe90a164d84b44d0594fbbf2ea93cb52263e31
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-19 20:15:07 +0200

    feat(WLLB-9): Create Card Detail List

--- app/components/Cards/CardDetails/CardDetailsTabs.tsx
+++ app/components/Cards/CardDetails/CardDetailsTabs.tsx
@@ -1,32 +1,30 @@
 export function CardDetailsTabs() {
   return (
-    <div className="self-stretch rounded-sm bg-text-brand-on-brand flex flex-col items-start justify-start py-[3px] px-[5px] text-center text-xs font-sf-pro">
-      <div className="w-[339px] rounded-sm h-[31px] overflow-hidden shrink-0 flex flex-row items-center justify-center">
-        <div className="flex flex-row items-center justify-center">
-          <div className="w-[77px] h-[32.5px]">
-            <div className=" tracking-[-0.23px] leading-[20px] font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap w-12 h-[32.5px]">
-              All
-            </div>
+    <div className="self-stretch rounded-lg bg-gainsboro-100 flex flex-col items-start justify-start p-1 text-center text-sm">
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="w-full">
+          <div className="tracking-1 leading-5 font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap">
+            All
           </div>
-          <div className="w-[77px] shadow-[0px_2px_16px_rgba(0,_0,_0,_0.08)] rounded-[100px] bg-miscellaneous-floating-tab-pill-fill h-[32.5px] text-text-brand-default">
-            <b className=" tracking-[-0.23px] leading-[20px] flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap w-12 h-[32.5px]">
-              Course
-            </b>
+        </div>
+        <div className="w-full shadow-[0px_2px_16px_rgba(0,_0,_0,_0.08)] rounded-[100px] bg-white h-6 flex items-center justify-center">
+          <b className="tracking-1 leading-5 flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap">
+            Course
+          </b>
+        </div>
+        <div className="w-full">
+          <div className="tracking-1 leading-5 font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap">
+            Practice
           </div>
-          <div className="w-[77px] h-[32.5px]">
-            <div className=" tracking-[-0.23px] leading-[20px] font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap w-12 h-[32.5px]">
-              Practice
-            </div>
+        </div>
+        <div className="w-full">
+          <div className="tracking-1 leading-5 font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap">
+            Books
           </div>
-          <div className="w-[77px] h-[32.5px]">
-            <div className=" tracking-[-0.23px] leading-[20px] font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap w-12 h-[32.5px]">
-              Books
-            </div>
-          </div>
-          <div className="w-[77px] h-[32.5px]">
-            <div className=" tracking-[-0.23px] leading-[20px] font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap w-12 h-[32.5px]">
-              Projects
-            </div>
+        </div>
+        <div className="w-full">
+          <div className="tracking-1 leading-5 font-medium flex items-center justify-center overflow-hidden text-ellipsis whitespace-nowrap">
+            Projects
           </div>
         </div>
       </div>
